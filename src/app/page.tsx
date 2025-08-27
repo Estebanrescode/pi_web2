f7a90f55de7ad634a6a0e5e4f7e81bffc89fcc86
Leandro Alberto Acevedo Zapata <lacevedoal93@gmail.com>
2025-08-26 20:19:07 -0500

    verificacion feacture-products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import CarouselTextBanner from "@/components/carousel-text-banner";
 import FeaturedProducts from "@/components/feature-products";
+import VistaProducto from "@/components/vista-producto";
 import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
    <main>
     <CarouselTextBanner />
     <FeaturedProducts/>
+    <VistaProducto/>
    </main>
   );
 }
--- src/components/feature-products.tsx
+++ src/components/feature-products.tsx
@@ -98,6 +98,7 @@ const FeaturedProducts = () => {
   );
 };
 
+
 export default FeaturedProducts;
 
 
